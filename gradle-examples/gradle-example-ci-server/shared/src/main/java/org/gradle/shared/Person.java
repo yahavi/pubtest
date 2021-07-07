1833b310dc34d3568eeca087d5f09e6d4fb1c67c
Nir Ovadia <ovadia.nir1@gmail.com>
2021-07-07 12:47:53 -0700

    Update Person.java

--- gradle-examples/gradle-example-ci-server/shared/src/main/java/org/gradle/shared/Person.java
+++ gradle-examples/gradle-example-ci-server/shared/src/main/java/org/gradle/shared/Person.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*to confirm 
 package org.gradle.shared;
 
 import java.io.IOException;
